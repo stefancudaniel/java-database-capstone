ea33fdd373e0526ef67e8f1ad3a4cdfb225efb54
stefancudaniel <stefancudaniel@gmail.com>
2025-12-16 15:54:01 +0200

    First version for models: Admin, Appointment, Doctor, Patient, Prescription

--- app/src/main/java/com/project/back_end/models/Doctor.java
+++ app/src/main/java/com/project/back_end/models/Doctor.java
@@ -1,5 +1,13 @@
 package com.project.back_end.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.List;
+@Entity
 public class Doctor {
 
 // @Entity annotation:
@@ -13,16 +21,15 @@ public class Doctor {
 //      - The @Id annotation marks it as the primary key.
 //      - The @GeneratedValue(strategy = GenerationType.IDENTITY) annotation auto-generates the ID value when a new record is inserted into the database.
 
-// 2. 'name' field:
+    // 2. 'name' field:
 //    - Type: private String
 //    - Description:
 //      - Represents the doctor's name.
 //      - The @NotNull annotation ensures that the doctor's name is required.
-//      - The @Size(min = 3, max = 100) annotation ensures that the name length is between 3 and 100 characters. 
+//      - The @Size(min = 3, max = 100) annotation ensures that the name length is between 3 and 100 characters.
 //      - Provides validation for correct input and user experience.
 
-
-// 3. 'specialty' field:
+    // 3. 'specialty' field:
 //    - Type: private String
 //    - Description:
 //      - Represents the medical specialty of the doctor.
@@ -60,6 +67,141 @@ public class Doctor {
 
 // 8. Getters and Setters:
 //    - Standard getter and setter methods are provided for all fields: id, name, specialty, email, password, phone, and availableTimes.
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "First name cannot be null")
+    @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
+    private String first_name;
+
+    @NotNull(message = "Last name cannot be null")
+    @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
+    private String last_name;
+
+    @NotNull(message = "Specialty cannot be null")
+    @Size(min = 3, max = 50, message = "Specialty must be between 3 and 50 characters")
+    private String specialty;
+
+    @NotNull(message = "Email cannot be null")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @Pattern(regexp = "\\d{10}")
+    private String phone;
+
+    @NotNull(message = "Password cannot be null")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+
+
+
+    @ElementCollection
+    private List<String> availableTimes;
+
+
+    private Long clinic_location_id;
+
+    @NotNull
+    private Boolean is_active;
+
+    @NotNull(message = "Creation date cannot be null")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private DateTimeFormat created_at;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<String> getAvailableTimes() {
+        return availableTimes;
+    }
+
+    public void setAvailableTimes(List<String> availableTimes) {
+        this.availableTimes = availableTimes;
+    }
+
+    public Long getClinic_location_id() {
+        return clinic_location_id;
+    }
+
+    public void setClinic_location_id(Long clinic_location_id) {
+        this.clinic_location_id = clinic_location_id;
+    }
+
+    public Boolean getIs_active() {
+        return is_active;
+    }
+
+    public void setIs_active(Boolean is_active) {
+        this.is_active = is_active;
+    }
+
+    public DateTimeFormat getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(DateTimeFormat created_at) {
+        this.created_at = created_at;
+    }
+
 
 }
 
--- app/src/main/java/com/project/back_end/models/Patient.java
+++ app/src/main/java/com/project/back_end/models/Patient.java
@@ -1,5 +1,17 @@
 package com.project.back_end.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
 public class Patient {
 // @Entity annotation:
 //    - Marks the class as a JPA entity, meaning it represents a table in the database.
@@ -11,6 +23,9 @@ public class Patient {
 //      - Represents the unique identifier for each patient.
 //      - The @Id annotation marks it as the primary key.
 //      - The @GeneratedValue(strategy = GenerationType.IDENTITY) annotation auto-generates the ID value when a new record is inserted into the database.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
 // 2. 'name' field:
 //    - Type: private String
@@ -20,6 +35,14 @@ public class Patient {
 //      - The @Size(min = 3, max = 100) annotation ensures that the name length is between 3 and 100 characters. 
 //      - Provides validation for correct input and user experience.
 
+    @NotNull(message = "First name cannot be null")
+    @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
+    private String first_name;
+
+    @NotNull(message = "Last name cannot be null")
+    @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
+    private String last_name;
+
 
 // 3. 'email' field:
 //    - Type: private String
@@ -27,6 +50,9 @@ public class Patient {
 //      - Represents the patient's email address.
 //      - The @NotNull annotation ensures that an email address must be provided.
 //      - The @Email annotation validates that the email address follows a valid email format (e.g., patient@example.com).
+    @NotNull(message = "Email cannot be null")
+    @Email(message = "Email should be valid")
+    private String email;
 
 // 4. 'password' field:
 //    - Type: private String
@@ -34,6 +60,9 @@ public class Patient {
 //      - Represents the patient's password for login authentication.
 //      - The @NotNull annotation ensures that a password must be provided.
 //      - The @Size(min = 6) annotation ensures that the password must be at least 6 characters long.
+    @NotNull(message = "Password cannot be null")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
 
 // 5. 'phone' field:
 //    - Type: private String
@@ -42,6 +71,10 @@ public class Patient {
 //      - The @NotNull annotation ensures that a phone number must be provided.
 //      - The @Pattern(regexp = "^[0-9]{10}$") annotation validates that the phone number must be exactly 10 digits long.
 
+    @NotNull(message = "Phone number cannot be null")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Phone number must be 10 digits")
+    private String phone;
+
 // 6. 'address' field:
 //    - Type: private String
 //    - Description:
@@ -49,10 +82,114 @@ public class Patient {
 //      - The @NotNull annotation ensures that the address must be provided.
 //      - The @Size(max = 255) annotation ensures that the address does not exceed 255 characters in length, providing validation for the address input.
 
+    @NotNull(message = "Address cannot be null")
+    @Size(max = 255, message = "Address cannot exceed 255 characters")
+    private String address;
+
+    @NotNull(message = "Date of Birth cannot be null")
+    private DateTimeFormat dateofbirth;
+
+    @NotNull(message = "Gender cannot be null")
+    private Integer gender;// 0 = Male, 1 = Female , 2 = Other
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @NotNull
+    private DateTimeFormat created_at;
+
+    @NotNull
+    private Boolean is_active = true;
 
 // 7. Getters and Setters:
 //    - Standard getter and setter methods are provided for all fields: id, name, email, password, phone, and address.
 //    - These methods allow access and modification of the fields of the Patient class.
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public DateTimeFormat getDateofbirth() {
+        return dateofbirth;
+    }
+
+    public void setDateofbirth(DateTimeFormat dateofbirth) {
+        this.dateofbirth = dateofbirth;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public DateTimeFormat getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(DateTimeFormat created_at) {
+        this.created_at = created_at;
+    }
+
+    public Boolean getIs_active() {
+        return is_active;
+    }
+
+    public void setIs_active(Boolean is_active) {
+        this.is_active = is_active;
+    }
 
   
 
